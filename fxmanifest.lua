c58ee6c1c93f6bc44a3646e6b76e261ce475317b
mark.ydr <51886957+markobn@users.noreply.github.com>
2023-04-26 01:48:49 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+
+fx_version 'cerulean'
+game 'gta5'
+
+data_file 'DLC_ITYP_REQUEST' 'stream/sandy_mechanic_metadata/sandy_mechanic.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/sandy_mechanic_metadata/sandy_mechanic_assets.ytyp'
+
+author 'mark.ydr'
+description 'Flywheels Mechanic MLO'
+version '1.0'
+
+this_is_a_map "yes"
+
+
